c56b88a1c71cbeeae65879e91d253d2abdbd8c8d
Duy Dat Pham <duydatpham@gmail.com>
2017-12-06 23:16:23 +0700

    init & clone project

---
+++ AndroidFloatSectionHeader.js
@@ -0,0 +1,39 @@
+import React, {
+    Component,
+} from 'react'
+import {
+    View,
+} from 'react-native'
+
+export default class AndroidFloatSectionHeader extends Component {
+
+    static propTypes = {
+        ...View.propTypes,
+    }
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            sectionID: '',
+            hidden: true,
+        }
+    }
+
+    render () {
+        return (
+            <View
+                {...this.props}
+                style={{position: 'absolute', top: 0, left: 0, width: this.props.floatSectionHeaderWidth,
+                        opacity: !this.state.hidden ? 1 : 0, }}>
+                {this.props.renderChildren(this.state.sectionID)}
+            </View>
+        )
+    }
+
+    setSectionID = (sectionID) => {
+        this.setState({
+            sectionID,
+        })
+    }
+
+}
---
+++ RefreshView.js
@@ -0,0 +1,40 @@
+import React, {
+    Component,
+} from 'react'
+import {
+    View,
+} from 'react-native'
+
+import constants from './constants'
+
+export default class RefreshView extends Component {
+
+    static defaultProps = {
+        renderRefreshContent: () => null,
+    }
+
+    static propTypes = {
+        ...View.propTypes,
+    }
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            pullState: props.viewType == constants.refreshViewType.header ? constants.viewState.refresh_none : constants.viewState.load_more_none,
+            pullDistancePercent: 0,
+        }
+    }
+
+    setNativeProps (props) {
+        this._refreshView.setNativeProps(props)
+    }
+
+    render () {
+        return (
+            <View ref={ component => this._refreshView = component } {...this.props}>
+                {this.props.renderRefreshContent(this.state)}
+            </View>
+        )
+    }
+
+}
